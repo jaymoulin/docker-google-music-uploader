5a76ea105e5357259f941e13676d02be89c32887
Jay MOULIN <jaymoulin@gmail.com>
2017-02-10 19:03:45 +0100

    Fix build with no cache

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 git clone http://github.com/jaymoulin/google-music-uploader.git
-docker build -t jaymoulin/rpi-google-music-uploader . --nocache=1
+docker build -t jaymoulin/rpi-google-music-uploader . --no-cache=true
