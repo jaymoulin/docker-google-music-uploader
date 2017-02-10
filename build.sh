8d05ac1dbc43a13a992a61a43a1cc8451e85e0f7
Jay MOULIN <jaymoulin@gmail.com>
2017-02-10 19:06:50 +0100

    Allow updatable dependency

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-git clone http://github.com/jaymoulin/google-music-uploader.git
+if [ ! -d google-music-uploader ]; then
+    git clone http://github.com/jaymoulin/google-music-uploader.git
+else
+    cd google-music-uploader && git pull && cd -
+fi
 docker build -t jaymoulin/rpi-google-music-uploader . --no-cache=true
