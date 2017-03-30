a3119f299d7c9e3c6f79672a1c63f281428e10c7
Jay MOULIN <jaymoulin@gmail.com>
2017-03-30 19:04:54 +0000

    fix builder

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ ! -d google-music-uploader ]; then
+if [ ! -d google-music-manager ]; then
     git clone http://github.com/jaymoulin/google-music-manager.git
 else
     cd google-music-manager && git pull && cd -
