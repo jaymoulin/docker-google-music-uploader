391f1772cb4c2d2f56a9d4f8a47be0d7a09bf0ad
Jay MOULIN <jaymoulin@gmail.com>
2017-05-23 13:51:53 +0200

    Fix build.sh to retrieve the latest version even with rebase

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 if [ ! -d google-music-manager ]; then
     git clone http://github.com/jaymoulin/google-music-manager.git
 else
-    cd google-music-manager && git pull && cd -
+    cd google-music-manager && git fetch && git reset --hard origin/master && cd -
 fi
 docker build -t jaymoulin/rpi-google-music-uploader . --no-cache=true
