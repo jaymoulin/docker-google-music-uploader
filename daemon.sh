0141959beda49be6b5322f8425f850920e343ab5
Jay MOULIN <jaymoulin@gmail.com>
2017-10-31 23:03:13 +0000

    Use latest goole music manager version
    Signed-off-by: Jay MOULIN <jaymoulin@gmail.com>

--- daemon.sh
+++ daemon.sh
@@ -9,7 +9,7 @@ if [ ! -f /root/oauth/oauth.key ]; then
     done
 else
     trap 'kill -TERM $PID' TERM INT
-    python3 /root/daemon.py /media/library /root/oauth/oauth.key True &
+    python3 /root/daemon.py -d /media/library -a /root/oauth/oauth.key -r &
     PID=$!
     wait $PID
     wait $PID
